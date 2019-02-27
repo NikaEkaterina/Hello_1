4d00a696fcf0bebfe5a165e23c74f4109865a9f6
NikaEkaterina <nika.e28@mail.ru>
2019-02-28 00:04:03 +0300

    задание_4

---
+++ src/Zadanie_4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Zadanie_4 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ввести число в бинарном формате: ");
+        String b = s.nextLine();
+        int Num = Integer.parseInt(b,);
+
+        System.out.println(Num);
+    }
+}
