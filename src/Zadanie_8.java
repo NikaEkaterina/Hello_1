d0e162c8ec26eedb3b37f0712eb80e7071d3266b
NikaEkaterina <nika.e28@mail.ru>
2019-03-03 22:05:39 +0300

    Zadanie_8

---
+++ src/Zadanie_8.java
@@ -0,0 +1,9 @@
+public class Zadanie_8 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i<50){
+            i++;
+            System.out.print(i+ " ");
+        }
+    }
+}
