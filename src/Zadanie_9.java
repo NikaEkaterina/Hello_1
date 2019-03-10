e67b1abdcf9e2f20394cb4824def892c0f244aee
NikaEkaterina <nika.e28@mail.ru>
2019-03-10 15:57:32 +0300

    Zadanie_9_изменения

--- src/Zadanie_9.java
+++ src/Zadanie_9.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 public class Zadanie_9 {
     public static void main(String[] args) {
-        int mas [] = new int [10];
-        for(int i=0; i<10;i++){
+        System.out.println("Введите размер массива");
+        Scanner n = new Scanner(System.in);
+        int N = n.nextInt();
+        int mas [] = new int [N];
+        System.out.println("Введите элементы массива");
+        for(int i=0; i<N;i++){
             Scanner t = new Scanner(System.in);
             mas [i]=t.nextInt();
         }
-        for (int i=0; i<10; i++){
+        for (int i=0; i<N; i++){
             System.out.print(mas [i]*2 + " ");
         }
     }
