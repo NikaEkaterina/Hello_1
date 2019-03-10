f71344924c0de72facb8704773ef8ba48cbcfeb5
NikaEkaterina <nika.e28@mail.ru>
2019-03-10 16:01:19 +0300

    Zadanie_11

---
+++ src/Zadanie_11.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Zadanie_11 {
+    public static void main(String[] args) {
+        System.out.println("Ввести любое слово или фразу: ");
+        Scanner str = new Scanner(System.in);
+        String st = str.nextLine();
+        System.out.println(st);
+
+    }
+}
